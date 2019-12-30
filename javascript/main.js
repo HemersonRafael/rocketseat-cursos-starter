9396bbc7c63625ac40c257a2f4d954f21b244597
HemersonRafael <rafaelpontes1995@gmail.com>
2019-12-30 01:30:29 -0300

    Atividade 20 do curso de javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,7 @@
-var minhaPromise = function(){
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', "https://api.github.com/users/HemersonRafael");
-        xhr.send(null);
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText))
-                } else{
-                    reject('erro na requisição')
-                }
-            }
-        }
 
-    });
-}
-
-//var resultado = minhaPromise();
-//console.log(resultado) pedding
-minhaPromise()
+axios.get("https://api.github.com/users/HemersonRafael")
     .then(function(response){
-        console.log(response)
+        console.log(response.data.avatar_url)
     })
     .catch(function(error){
         console.warn(error)
