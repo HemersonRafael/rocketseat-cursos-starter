9046864c43431055a3471fd536bf387ff3225271
HemersonRafael <rafaelpontes1995@gmail.com>
2019-12-30 00:34:55 -0300

    Atividade 15 do curso de javascript

--- javascript/todos.js
+++ javascript/todos.js
@@ -8,3 +8,23 @@ var todos = [
     'acessar comunidade da rocketseat'
 ];
 
+function renderTodos(){
+    listElement.innerHTML = '';
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo(){
+    var todoText = inputElement.value;
+    todos.push(todoText)
+    inputElement.value = '';
+    renderTodos();
+}
+buttonElement.onclick = addTodo;
