4c1a0be53cd92f7b5d24d669e225a27f78581e4b
HemersonRafael <rafaelpontes1995@gmail.com>
2019-12-30 00:22:19 -0300

    Atividade 14 do curso de javascript

---
+++ javascript/todos.js
@@ -0,0 +1,10 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+
+var todos = [
+    'Fazer café',
+    'estudar javascript',
+    'acessar comunidade da rocketseat'
+];
+
