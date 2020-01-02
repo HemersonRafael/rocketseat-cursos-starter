d49b4ed3b46d7f29ece1bedbd1c20a31a1c63f15
HemersonRafael <rafaelpontes1995@gmail.com>
2020-01-02 01:26:38 -0300

    desafio EX3 do curso de javascript

--- javascript/desafio.js
+++ javascript/desafio.js
@@ -5,8 +5,13 @@ var ulElement = document.querySelector('ul[name=list]');
 
 function getRepos(){
     var user = inputElement.value;
+    var liElement = document.createElement('li');
+    var liText = document.createTextNode("Carregando");
+    liElement.appendChild(liText);
+    ulElement.appendChild(liElement);
     axios.get("https://api.github.com/users/"+ user + "/repos")
     .then(function(response){
+        ulElement.innerHTML = '';
         for(var i = 0; i < response.data.length; i++){
             var name = response.data[i].name;
             //console.log(name)
@@ -18,6 +23,7 @@ function getRepos(){
     })
     .catch(function(error){
         console.warn(error)
+        alert("usuário inválido!")
     });
 }
 
