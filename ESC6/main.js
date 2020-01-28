6bb3ca9b7bd5da74ab147e5109148a18d62b5517
HemersonRafael <rafaelpontes1995@gmail.com>
2020-01-27 23:26:14 -0300

    map, reduce, filter and find

--- ESC6/main.js
+++ ESC6/main.js
@@ -1,21 +1,25 @@
-class List {
-    constructor(){
-        this.data = [];
-    }
-    add(data){
-        this.data.push(data);
-        console.log(this.data)
-    }
-}
-class TodoList extends List{
-    constructor(){
-        super();//chamada para construtor da class pai
-        this.usuario = "hemerspn";
-    }   
-}
+const arr = [1,3,4,5,8,9]
 
-const MinhaLista = new TodoList();
-const valor = document.getElementById('valor').value
-document.getElementById('novotodo').onclick = function(){
-    MinhaLista.add(document.getElementById('valor').value);
-}
+const newArr = arr.map(function(item, index){
+    return item + index;
+});
+
+console.log(newArr)
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+})
+
+console.log(sum)
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 4;
+});
+
+console.log(find);
