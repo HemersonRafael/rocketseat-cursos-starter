9aefc81dc1df0e16d6d5032f524c4654e2155950
HemersonRafael <rafaelpontes1995@gmail.com>
2020-01-28 00:12:44 -0300

    Operadores rest/spread

--- ESC6/main.js
+++ ESC6/main.js
@@ -1,20 +1,38 @@
-const usuario = {
-    nome: 'Hemerson',
-    idade: 24,
-    endereco: {
-        cidade: "Natal",
-        estado: "RN"
-    },
-}
+//rest
+//pega o resto das propiedades
 
-const {nome, idade, endereco:{ cidade}} = usuario;
+const usuario ={
+    nome: "Hemerson",
+    idade: 23,
+    empresa: "ufrn"
+};
+
+const {nome, ...resto} = usuario;
 
 console.log(nome);
-console.log(idade);
-console.log(cidade);
+console.log(resto);
 
-function mostrar({nome, idade}){
-    console.log(nome, idade);
-}
+const arr = [1, 2, 3, 4];
 
-mostrar(usuario)
+const [a, b, ...c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma(...params){
+    return params.reduce((total, next) => total + next);
+};
+
+console.log(soma(1, 2, 3, 4, 5));
+
+//spread
+//tem  o papel de propagar
+
+const arr1 = [1, 2, 3]
+const arr2 = [4, 5, 6]
+
+const arr3 = [...arr1, ...arr2]
+console.log(arr3)
+
+const usuario2 = {...usuario, nome: 'Gabriel'};
