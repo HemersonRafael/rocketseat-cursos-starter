7156731570dde2af55d27f5426966db79085c4bf
HemersonRafael <rafaelpontes1995@gmail.com>
2020-01-29 11:50:49 -0300

    import/export

--- ESC6/funcoes.js
+++ ESC6/funcoes.js
@@ -5,3 +5,7 @@ export function soma (a,b){
 export function sub(a, b){
     return a - b;
 };
+
+export function mult(a, b){
+    return a * b;
+};
--- ESC6/main.js
+++ ESC6/main.js
@@ -1,4 +1,7 @@
-import { soma, sub } from './funcoes'
-
-console.log(soma(1,2));
-console.log(sub(2,1));
+//import { soma, sub } from './funcoes'
+//import {soma as somaFunction, sub} from './funcoes'
+//import somaFunction from './soma'
+import * as funcoes from './funcoes'
+console.log(funcoes.soma(1,2));
+console.log(funcoes.sub(2,1));
+console.log(funcoes.mult(2,3));
