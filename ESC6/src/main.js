8b0e0dd9ea4f67b29d34a7fd1481e874cacd1f19
HemersonRafael <rafaelpontes1995@gmail.com>
2020-01-29 12:20:54 -0300

    async/await

--- ESC6/src/main.js
+++ ESC6/src/main.js
@@ -1,1 +1,26 @@
-alert('testando!!')
+const minhaPromise = () =>  new Promise((resolve, reject)=>{
+    setTimeout(() => {resolve('ok'), 2000});
+});
+
+/*minhaPromise()
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err =>{
+
+    });
+*/
+
+/*async function executaPromisse(){
+    console.log( await minhaPromise());
+    console.log( await minhaPromise());
+    console.log( await minhaPromise());
+}
+*/
+const executaPromisse = async () =>{
+    console.log( await minhaPromise());
+    console.log( await minhaPromise());
+    console.log( await minhaPromise());
+};
+
+executaPromisse();
