cb7f6a56f8a6c2e7c5a593f31868712309950ac0
HemersonRafael <rafaelpontes1995@gmail.com>
2020-01-29 14:54:50 -0300

    desafio EX1

--- ESC6/webpack.config.js
+++ ESC6/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: ['@babel/polyfill', './src/main.js'],
+    entry: ['@babel/polyfill', './src/desafio1.js'],
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js',
