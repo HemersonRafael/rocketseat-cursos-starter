5cf105cd014ba8a943b859fd8ab6f2f980b367b3
HemersonRafael <rafaelpontes1995@gmail.com>
2020-01-29 12:03:06 -0300

    webpack dev server

--- ESC6/main.js
+++
@@ -1,7 +0,0 @@
-//import { soma, sub } from './funcoes'
-//import {soma as somaFunction, sub} from './funcoes'
-//import somaFunction from './soma'
-import * as funcoes from './funcoes'
-console.log(funcoes.soma(1,2));
-console.log(funcoes.sub(2,1));
-console.log(funcoes.mult(2,3));
